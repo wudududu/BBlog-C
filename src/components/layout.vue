550b727e188d83345845bb222657d375d719e698
wudu <wd@scsvision.com>
2018-12-27 17:36:00 +0800

    引入markdown解析模块，为展示文章做准备

---
+++ src/components/layout.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div id="header">
+      <span>BBlog</span>
+    </div>
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'layout',
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+#header {
+  width: 100%;
+  height: 60px;
+  box-shadow: 0px 2px #f1f1f1
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import layout from '../components/layout.vue'
 
 Vue.use(Router)
 
@@ -8,8 +8,13 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'layout',
+      component: layout,
+      children: [{
+        path: '/contents',
+        name: 'contents',
+        component: () => import('../components/content.vue')
+      }]
     }
   ]
 })
